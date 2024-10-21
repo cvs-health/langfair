1facffe7cb0e78bc14bb924d8933b0de09ca04f4
Dylan Bouchard <dylan.bouchard@cvshealth.com>
2024-10-21 20:34:10 +0000

    update unit test

--- tests/test_counterfactual_metrics.py
+++ tests/test_counterfactual_metrics.py
@@ -47,5 +47,5 @@ def test_CounterfactualMetrics():
     ans = actual_results["test6"]
     assert all([
         abs(score[key]-ans[key])<1e-5 for key in ans
-        if key !- "Cosine Similarity"
+        if key != "Cosine Similarity"
     ])
