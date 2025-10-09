7998039583362b20c3cd7200c3f31014b2a22d54
Zeya Ahmad <za2291@columbia.edu>
2025-10-09 17:08:04 -0400

    mock progress bar to fix LiveError

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,6 +5,7 @@ import pytest
 
 @pytest.fixture(autouse=True)
 def patch_progress_bar(monkeypatch):
+    # Original source of progress bar
     import langfair.utils.display as display_module
 
     monkeypatch.setattr(
@@ -12,4 +13,106 @@ def patch_progress_bar(monkeypatch):
     )
     monkeypatch.setattr(
         display_module, "stop_progress_bar", lambda *args, **kwargs: None
+    )
+
+    # All modules that imported progress bar  directly
+    import langfair.auto.auto as auto_module
+    import langfair.generator.counterfactual as generator_cf_module
+    import langfair.generator.generator as generator_module
+    import langfair.metrics.counterfactual.counterfactual as counterfactual_module
+    import langfair.metrics.counterfactual.metrics.bleu as bleu_module
+    import langfair.metrics.counterfactual.metrics.cosine as cosine_module
+    import langfair.metrics.counterfactual.metrics.rougel as rougel_module
+    import langfair.metrics.counterfactual.metrics.sentimentbias as sentimentbias_module
+    import langfair.metrics.stereotype.metrics.associations as associations_module
+    import langfair.metrics.stereotype.metrics.classifier as classifier_module
+    import langfair.metrics.stereotype.metrics.cooccurrence as cooccurrence_module
+    import langfair.metrics.stereotype.stereotype as stereotype_module
+    import langfair.metrics.toxicity.toxicity as toxicity_module
+
+    monkeypatch.setattr(
+        classifier_module, "start_progress_bar", lambda *args, **kwargs: MagicMock()
+    )
+    monkeypatch.setattr(
+        classifier_module, "stop_progress_bar", lambda *args, **kwargs: None
+    )
+
+    monkeypatch.setattr(
+        associations_module, "start_progress_bar", lambda *args, **kwargs: MagicMock()
+    )
+    monkeypatch.setattr(
+        associations_module, "stop_progress_bar", lambda *args, **kwargs: None
+    )
+
+    monkeypatch.setattr(
+        cooccurrence_module, "start_progress_bar", lambda *args, **kwargs: MagicMock()
+    )
+    monkeypatch.setattr(
+        cooccurrence_module, "stop_progress_bar", lambda *args, **kwargs: None
+    )
+
+    monkeypatch.setattr(
+        stereotype_module, "start_progress_bar", lambda *args, **kwargs: MagicMock()
+    )
+    monkeypatch.setattr(
+        stereotype_module, "stop_progress_bar", lambda *args, **kwargs: None
+    )
+
+    monkeypatch.setattr(
+        toxicity_module, "start_progress_bar", lambda *args, **kwargs: MagicMock()
+    )
+    monkeypatch.setattr(
+        toxicity_module, "stop_progress_bar", lambda *args, **kwargs: None
+    )
+
+    monkeypatch.setattr(
+        cosine_module, "start_progress_bar", lambda *args, **kwargs: MagicMock()
+    )
+    monkeypatch.setattr(
+        cosine_module, "stop_progress_bar", lambda *args, **kwargs: None
+    )
+
+    monkeypatch.setattr(
+        sentimentbias_module, "start_progress_bar", lambda *args, **kwargs: MagicMock()
+    )
+    monkeypatch.setattr(
+        sentimentbias_module, "stop_progress_bar", lambda *args, **kwargs: None
+    )
+
+    monkeypatch.setattr(
+        bleu_module, "start_progress_bar", lambda *args, **kwargs: MagicMock()
+    )
+    monkeypatch.setattr(bleu_module, "stop_progress_bar", lambda *args, **kwargs: None)
+
+    monkeypatch.setattr(
+        rougel_module, "start_progress_bar", lambda *args, **kwargs: MagicMock()
+    )
+    monkeypatch.setattr(
+        rougel_module, "stop_progress_bar", lambda *args, **kwargs: None
+    )
+
+    monkeypatch.setattr(
+        counterfactual_module, "start_progress_bar", lambda *args, **kwargs: MagicMock()
+    )
+    monkeypatch.setattr(
+        counterfactual_module, "stop_progress_bar", lambda *args, **kwargs: None
+    )
+
+    monkeypatch.setattr(
+        auto_module, "start_progress_bar", lambda *args, **kwargs: MagicMock()
+    )
+    monkeypatch.setattr(auto_module, "stop_progress_bar", lambda *args, **kwargs: None)
+
+    monkeypatch.setattr(
+        generator_cf_module, "start_progress_bar", lambda *args, **kwargs: MagicMock()
+    )
+    monkeypatch.setattr(
+        generator_cf_module, "stop_progress_bar", lambda *args, **kwargs: None
+    )
+
+    monkeypatch.setattr(
+        generator_module, "start_progress_bar", lambda *args, **kwargs: MagicMock()
+    )
+    monkeypatch.setattr(
+        generator_module, "stop_progress_bar", lambda *args, **kwargs: None
     )
